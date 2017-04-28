212ce6ee197422053d04260545a46a05fa1fc2e4
xzjs <2310976219@163.com>
2017-04-28 16:02:24 +0800

    放置模型

---
+++ Assets/Script/Click.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Assets.Script
+{
+    public class Click : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Transform shelf = hit.transform;
+                    if (shelf.tag != "Shelf")
+                    {
+                        shelf = shelf.parent;
+                    }
+
+                    Debug.Log(shelf.gameObject.name);
+                }
+            }
+        }
+    }
+}

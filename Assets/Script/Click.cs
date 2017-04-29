55e9b250f4f19222c9db33de8b577a0496240ea0
xzjs <2310976219@qq.com>
2017-04-29 22:14:50 +0800

    完成部分UI 完成摁Tab键脱离控制的功能

--- Assets/Script/Click.cs
+++ Assets/Script/Click.cs
@@ -4,6 +4,13 @@ namespace Assets.Script
 {
     public class Click : MonoBehaviour
     {
+        public Shader RimLightShader;
+        public Color RimColor = new Color(0.2f, 0.8f, 10.6f, 1);
+
+        private MeshRenderer _mesh;
+        private Color _color;
+        private Shader _shader;
+
         // Use this for initialization
         void Start()
         {
@@ -24,8 +31,17 @@ namespace Assets.Script
                     {
                         shelf = shelf.parent;
                     }
-
                     Debug.Log(shelf.gameObject.name);
+
+                    foreach (Transform childTransform in shelf)
+                    {
+                        _mesh = childTransform.gameObject.GetComponentInChildren<MeshRenderer>();
+
+                        _shader = _mesh.material.shader;
+
+                        _mesh.material.shader = RimLightShader;
+                        _mesh.material.SetColor("_RimColor", RimColor);
+                    }
                 }
             }
         }
---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject FPSController;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown("tab"))
+	    {
+	        FirstPersonController first = FPSController.GetComponent<FirstPersonController>();
+	        first.enabled = !first.enabled;
+	    }
+	}
+}

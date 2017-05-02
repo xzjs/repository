d165ef9760b98ac14cb6d7276e7649ab9119864e
xzjs <2310976219@163.com>
2017-05-02 14:45:29 +0800

    添加获取数据功能 添加解析json功能

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -1,34 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Assets.Script;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
 
 public class GameController : MonoBehaviour
 {
     public GameObject FpsController, MainCamera;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    if (Input.GetKeyDown("tab"))
-	    {
-	        FirstPersonController first = FpsController.GetComponent<FirstPersonController>();
-	        first.enabled = !first.enabled;
-	        if (Cursor.lockState == CursorLockMode.Locked)
-	        {
-	            Cursor.lockState = CursorLockMode.None;
-	            Cursor.visible = true;
-	        }
-	        else
-	        {
-	            Cursor.lockState=CursorLockMode.Locked;
-	            Cursor.visible = false;
-	        }
-	    }
-	}
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(GetData("http://repository.xzjs.love/api/shelfs"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("tab"))
+        {
+            FirstPersonController first = FpsController.GetComponent<FirstPersonController>();
+            first.enabled = !first.enabled;
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+    }
 
     /// <summary>
     /// 切换视角
@@ -42,8 +45,41 @@ public class GameController : MonoBehaviour
         }
         else
         {
-            MainCamera.SetActive(true);;
+            MainCamera.SetActive(true); ;
             FpsController.SetActive(false);
+        }
+    }
+
+    private IEnumerator GetData(string url)
+    {
+        WWW getData = new WWW("http://repository.xzjs.love/api/shelfs");
+        yield return getData;
+        if (getData.error != null)
+        {
+            Debug.Log(getData.error);
+        }
+        else
+        {
+            Debug.Log(getData.text);
+            var shelfs = JsonHelper.FromJson<Shelf>("{\"Items\":" + getData.text + "}");
+            foreach (var shelf in shelfs)
+            {
+                foreach (var floor in shelf.floors)
+                {
+                    foreach (var cell in floor.cells)
+                    {
+                        var good = cell.good;
+                        if (good == null) continue;
+                        GameObject g = Resources.Load("Goods/" + good.model_name) as GameObject;
+                        var c = GameObject.Find(string.Format("{0}-{1}-{2}", shelf.no, floor.no, cell.no)).transform;
+                        var clone = Instantiate(g, c).transform;
+
+                        clone.position = c.position;
+                        //clone.parent = c;
+                        //clone.localPosition = new Vector3();
+                    }
+                }
+            }
         }
     }
 }
---
+++ Assets/Script/ShelfInfo.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Script
+{
+    [Serializable]
+    public class Good
+    {
+        public int id;
+        public string name;
+        public string model_name;
+        public string unit;
+        public int num;
+        public int cell_id;
+    }
+
+    [Serializable]
+    public class Cell
+    {
+        public int id;
+        public int no;
+        public int floor_id;
+        public Good good;
+    }
+
+    [Serializable]
+    public class Floor
+    {
+        public int id;
+        public int no;
+        public int shelf_id;
+        public Cell[] cells;
+    }
+
+    [Serializable]
+    public class Shelf
+    {
+        public int id;
+        public int no;
+        public int type;
+        public Floor[] floors;
+        public string created_at;
+        public string updated_at;
+    }
+
+    public static class JsonHelper
+    {
+        public static T[] FromJson<T>(string json)
+        {
+            Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+            return wrapper.Items;
+        }
+
+        public static string ToJson<T>(T[] array)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper);
+        }
+
+        public static string ToJson<T>(T[] array, bool prettyPrint)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper, prettyPrint);
+        }
+
+        [Serializable]
+        private class Wrapper<T>
+        {
+            public T[] Items;
+        }
+    }
+}

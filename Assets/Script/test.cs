d79038a1a199e89f0a6eed54e10a260d6d322f42
xzjs <2310976219@qq.com>
2017-05-14 12:54:47 +0800

    测试回调函数

---
+++ Assets/Script/test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Script
+{
+    public class test : MonoBehaviour
+    {
+
+        public UILabel Label;
+        public void Click()
+        {
+            Application.ExternalCall("test","hello world");
+        }
+
+        public void Change(string str)
+        {
+            Label.text = str;
+        }
+    }
+}
